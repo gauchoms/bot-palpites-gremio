9334ca9f0c867409ece7edccc108fe91ad1d7807
Ricardo Feldens <ricardo.feldens@gmail.com>
2025-04-06 17:45:29 -0300

    verifica o atualizar jogos - especificamente pq a api retorna vazios

--- atualizar_jogos.js
+++ atualizar_jogos.js
@@ -1,17 +1,3 @@
-import axios from 'axios';
-import { GoogleAuth } from 'google-auth-library';
-import { google } from 'googleapis';
-
-// Corrigido: nome da variável de ambiente está em maiúsculo
-const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_CREDENTIALS_B64, 'base64').toString('utf-8'));
-
-const auth = new GoogleAuth({
-  credentials,
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
-
-const sheets = google.sheets({ version: 'v4', auth });
-
 async function atualizarPlanilha() {
   try {
     const response = await axios.get('https://v3.football.api-sports.io/fixtures?team=130&next=10', {
@@ -19,6 +5,10 @@ async function atualizarPlanilha() {
         'x-apisports-key': process.env.APIFOOTBALL_KEY,
       },
     });
+
+    // 🐛 Adicionado: printa a resposta crua da API
+    console.log('Resposta bruta da API-Football:');
+    console.log(JSON.stringify(response.data, null, 2));
 
     const jogos = response.data.response.map((jogo) => {
       const data = new Date(jogo.fixture.date);
@@ -29,6 +19,7 @@ async function atualizarPlanilha() {
         jogo.league.name,
       ];
     });
+
     console.log('Jogos a serem inseridos na planilha:');
     console.log(JSON.stringify(jogos, null, 2));
     
@@ -46,5 +37,3 @@ async function atualizarPlanilha() {
     console.error('Erro ao atualizar planilha:', error);
   }
 }
-
-atualizarPlanilha();
